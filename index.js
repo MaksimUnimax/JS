b8e7f14b4e8a167aee055134bd1ef93fb0f25095
Maksim <unymax2014@gmail.com>
2019-04-26 08:42:05 +0300

    JS

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const productPrice = [200, 550, 4000, 23, 58, 5000, 485, 711];
+let user;
+let bonus;
+
+function calcBonus(price) {
+  let sum = 0;
+  let endPrice;
+  for (let value of price) {
+    sum += value;
+  }
+  if (sum > 10000) {
+    endPrice = (sum * 5)/100;
+  }
+  return Math.round(endPrice);
+}
+
+console.log(calcBonus(productPrice));
